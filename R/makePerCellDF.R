8d3e54daf81890fdacc6943dad49935d2bab6e7f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-01 21:30:02 -0700

    Swapped underscores for dots in makePer*DF functions. Added arguments to control inclusion of row/colData.

--- R/makePerCellDF.R
+++ R/makePerCellDF.R
@@ -1,4 +1,4 @@
-#' Create a per-cell data.frame from a SingleCellDataFrame
+#' Create a per-cell data.frame 
 #'
 #' Create a per-cell data.frame (i.e., where each row represents a cell) from a \linkS4class{SingleCellExperiment},
 #' most typically for creating custom \pkg{ggplot2} plots.
@@ -7,14 +7,18 @@
 #' This is expected to have non-\code{NULL} row names.
 #' @param features Character vector specifying the features for which to extract expression profiles across cells.
 #' May also include features in alternative Experiments if permitted by \code{use_altexps}.
-#' @param exprs_values String or integer scalar indicating the assay to use to obtain expression values.
+#' @param assay.type String or integer scalar indicating the assay to use to obtain expression values.
 #' Must refer to a matrix-like object with integer or numeric values.
-#' @param use_altexps Logical scalar indicating whether (meta)data should be extracted for alternative experiments in \code{x}.
+#' @param use.coldata Logical scalar indicating whether column metadata of \code{x} should be included.
+#' Alternatively, a character or integer vector specifying the column metadata fields to use.
+#' @param use.altexps Logical scalar indicating whether (meta)data should be extracted for alternative experiments in \code{x}.
 #' Alternatively, a character or integer vector specifying the alternative experiments to use. 
-#' @param use_dimred Logical scalar indicating whether data should be extracted for dimensionality reduction results in \code{x}.
+#' @param use.dimred Logical scalar indicating whether data should be extracted for dimensionality reduction results in \code{x}.
 #' Alternatively, a character or integer vector specifying the dimensionality reduction results to use.
-#' @param prefix_altexps Logical scalar indicating whether \code{\link{altExp}}-derived fields should be prefixed with the name of the alternative Experiment.
-#' @param check_names Logical scalar indicating whether the column names of the output data.frame should be made syntactically valid and unique.
+#' @param prefix.altexps Logical scalar indicating whether \code{\link{altExp}}-derived fields should be prefixed with the name of the alternative Experiment.
+#' @param check.names Logical scalar indicating whether column names of the output should be made syntactically valid and unique.
+#' @param exprs_values,use_dimred,use_altexps,prefix_altexps,check_names
+#' Soft-deprecated equivalents of the arguments described above.
 #'
 #' @return A data.frame containing one field per aspect of data in \code{x} - see Details.
 #' Each row corresponds to a cell (i.e., column) of \code{x}.
@@ -23,51 +27,63 @@
 #' This function enables us to conveniently create a per-feature data.frame from a \linkS4class{SingleCellExperiment}.
 #' Each row of the returned data.frame corresponds to a column in \code{x},
 #' while each column of the data.frame corresponds to one aspect of the (meta)data in \code{x}.
+#'
 #' Columns are provided in the following order:
 #' \enumerate{
-#' \item Columns named according to the values in \code{features} represent the expression values across cells for the specified feature in the \code{exprs_values} assay.
-#' \item Columns named according to the columns of \code{rowData(x)} represent the row metadata variables.
-#' \item If \code{use_dimred=TRUE}, columns named in the format of \code{<DIM>.<NUM>} represent the \code{<NUM>}th dimension of the dimensionality reduction result \code{<DIM>}.
-#' \item If \code{use_altexps=TRUE}, columns are named according to the row names and column metadata fields of successive alternative Experiments,
-#' representing the assay data and metadata respectively in these objects.
-#' The names of these columns are prefixed with the name of the alternative Experiment if \code{prefix_altexps=TRUE}.
-#' Note that alternative Experiment rows will only be present if they are specified in \code{features}.
+#' \item Columns named according to the entries of \code{features} represent the expression values across cells for the specified feature in the \code{assay.type} assay.
+#' \item Columns named according to the columns of \code{colData(x)} represent column metadata variables.
+#' This consists of all variables if \code{use.coldata=TRUE}, no variables if \code{use.coldata=FALSE},
+#' and only the specified variables if \code{use.coldata} is set to an integer or character vector.
+#' \item Columns named in the format of \code{<DIM>.<NUM>} represent the \code{<NUM>}th dimension of the dimensionality reduction result \code{<DIM>}.
+#' This is generated for all dimensionality reduction results if \code{use.dimred=TRUE}, none if \code{use.dimred=FALSE},
+#' and only the specified results if \code{use.dimred}is set to an integer or character vector.
+#' \item Columns named according to the row names of successive alternative Experiments,
+#' representing the assay data in these objects.
+#' These columns are only included if they are specified in \code{features} and if \code{use.altexps} is set.
+#' Column names are prefixed with the name of the alternative Experiment if \code{prefix.altexps=TRUE}.
 #' }
 #'
-#' By default, nothing is done to resolve syntactically invalid or duplicated column names;
-#' this will often lead (correctly) to an error in downstream functions like \code{\link{ggplot}}.
-#' If \code{check_names=TRUE}, this is resolved by passing the column names through \code{\link{make.names}}.
+#' By default, nothing is done to resolve syntactically invalid or duplicated column names.
+#' \code{check_names=TRUE}, this is resolved by passing the column names through \code{\link{make.names}}.
 #' Of course, as a result, some columns may not have the same names as the original fields in \code{x}.
 #'
 #' @author Aaron Lun
 #'
 #' @seealso
-#' \code{\link{ggcells}}, which uses this function under the hood.
+#' \code{\link{makePerFeatureDF}}, for the feature-level equivalent.
 #'
 #' @examples
-#' example_sce <- mockSCE()
-#' example_sce <- logNormCounts(example_sce)
-#' example_sce <- runPCA(example_sce)
+#' sce <- mockSCE()
+#' sce <- logNormCounts(sce)
+#' reducedDim(sce, "PCA") <- matrix(rnorm(ncol(sce)*10), ncol=10) # made-up PCA.
 #'
-#' df <- makePerCellDF(example_sce, features="Gene_0001")
-#' head(colnames(df))
-#' tail(colnames(df))
-#'
-#' df$Gene_0001
-#' df$Mutation_Status
-#' df$PCA.1
+#' df <- makePerCellDF(sce, features="Gene_0001")
+#' head(df)
 #' 
 #' @export
-#' @importFrom SingleCellExperiment reducedDims reducedDimNames altExps altExpNames
-makePerCellDF <- function(x, features=NULL, exprs_values="logcounts", 
-    use_dimred=TRUE, use_altexps=FALSE, prefix_altexps=FALSE, check_names=FALSE) 
+#' @importFrom SingleCellExperiment colData reducedDims reducedDimNames altExps altExpNames
+makePerCellDF <- function(x, features=NULL, assay.type="logcounts", 
+    use.coldata=TRUE, use.dimred=TRUE, use.altexps=TRUE, prefix.altexps=FALSE, check.names=FALSE,
+    exprs_values=NULL, use_dimred=NULL, use_altexps=NULL, prefix_altexps=NULL, check_names=NULL)
 {
-    output <- list(.harvest_se_by_column(x, features=features, exprs_values=exprs_values))
+    use.dimred <- .replace(use.dimred, use_dimred)
+    use.altexps <- .replace(use.altexps, use_altexps)
+    assay.type <- .replace(assay.type, exprs_values)
+    prefix.altexps <- .replace(prefix.altexps, prefix_altexps)
+
+    output <- list(.harvest_se_by_column(x, features=features, assay.type=assay.type))
+
+    # Collecting the column metadata.
+    use.coldata <- .use_names_to_integer_indices(use.coldata, x=x, nameFUN=function(x) colnames(colData(x)), msg="use.coldata")
+    if (length(use.coldata)) {
+        cd <- colData(x)[,use.coldata,drop=FALSE]
+        output <- c(output, list(as.data.frame(cd)))
+    }
 
     # Collecting the reduced dimensions.
-    use_dimred <- .use_names_to_integer_indices(use_dimred, x=x, nameFUN=reducedDimNames, msg="use_dimred")
-    if (length(use_dimred)) {
-        all_reds <- reducedDims(x)[use_dimred]
+    use.dimred <- .use_names_to_integer_indices(use.dimred, x=x, nameFUN=reducedDimNames, msg="use.dimred")
+    if (length(use.dimred)) {
+        all_reds <- reducedDims(x)[use.dimred]
         red_vals <- vector("list", length(all_reds))
 
         for (r in seq_along(red_vals)) {
@@ -81,14 +97,14 @@ makePerCellDF <- function(x, features=NULL, exprs_values="logcounts",
     }
 
     # Collecting the alternative Experiments.
-    use_altexps <- .use_names_to_integer_indices(use_altexps, x=x, nameFUN=altExpNames, msg="use_altexps")
-    if (length(use_altexps)) {
-        all_alts <- altExps(x)[use_altexps]
+    use.altexps <- .use_names_to_integer_indices(use.altexps, x=x, nameFUN=altExpNames, msg="use.altexps")
+    if (length(use.altexps)) {
+        all_alts <- altExps(x)[use.altexps]
         alt_vals <- vector("list", length(all_alts))
 
         for (a in seq_along(alt_vals)) {
-            curalt <- .harvest_se_by_column(all_alts[[a]], features=features, exprs_values=exprs_values)
-            if (prefix_altexps) {
+            curalt <- .harvest_se_by_column(all_alts[[a]], features=features, assay.type=assay.type)
+            if (prefix.altexps) {
                 colnames(curalt) <- sprintf("%s.%s", names(all_alts)[a], colnames(curalt))
             }
             alt_vals[[a]] <- curalt
@@ -98,23 +114,26 @@ makePerCellDF <- function(x, features=NULL, exprs_values="logcounts",
         output <- c(output, list(alt_vals))
     }
 
+    # Checking the names.
     output <- do.call(cbind, output)
-    if (check_names) {
+    if (check.names) {
         colnames(output) <- make.names(colnames(output), unique=TRUE)
     }
     output
 }
 
-#' @importFrom SummarizedExperiment assay colData
+#' @importFrom SummarizedExperiment assay
 #' @importFrom Matrix t
-.harvest_se_by_column <- function(x, features, exprs_values) {
-    # Collecting the assay values.
+.harvest_se_by_column <- function(x, features, assay.type) {
     keep <- rownames(x) %in% features
-    curmat <- assay(x, exprs_values, withDimnames=FALSE)[keep,,drop=FALSE]
-    curmat <- as.matrix(t(curmat))
-    assay_vals <- data.frame(curmat, row.names=colnames(x))
-    colnames(assay_vals) <- rownames(x)[keep]
-
-    # Adding column metadata.
-    cbind(assay_vals, colData(x))
+    if (any(keep)) {
+        curmat <- assay(x, assay.type, withDimnames=FALSE)[keep,,drop=FALSE]
+        curmat <- as.matrix(t(curmat))
+        assay_vals <- data.frame(curmat, row.names=colnames(x))
+        colnames(assay_vals) <- rownames(x)[keep]
+        assay_vals
+    } else {
+        # Avoid throwing an error for altexps if the feature doesn't even match.
+        data.frame(matrix(0, ncol(x), 0L), row.names=colnames(x))
+    }
 }
--- R/makePerFeatureDF.R
+++ R/makePerFeatureDF.R
@@ -1,4 +1,4 @@
-#' Create a per-feature data.frame from a SingleCellDataFrame
+#' Create a per-feature data.frame 
 #'
 #' Create a per-feature data.frame (i.e., where each row represents a feature) from a \linkS4class{SingleCellExperiment},
 #' most typically for creating custom \pkg{ggplot2} plots.
@@ -6,9 +6,13 @@
 #' @param x A \linkS4class{SingleCellExperiment} object.
 #' This is expected to have non-\code{NULL} row names.
 #' @param cells Character vector specifying the features for which to extract expression profiles across cells.
-#' @param exprs_values String or integer scalar indicating the assay to use to obtain expression values.
+#' @param use.rowdata Logical scalar indicating whether row metadata of \code{x} should be included.
+#' Alternatively, a character or integer vector specifying the row metadata fields to use.
+#' @param assay.type String or integer scalar indicating the assay to use to obtain expression values.
 #' Must refer to a matrix-like object with integer or numeric values.
-#' @param check_names Logical scalar indicating whether the column names of the output data.frame should be made syntactically valid and unique.
+#' @param check.names Logical scalar indicating whether column names of the output should be made syntactically valid and unique.
+#' @param use_exprs_values,check_names 
+#' Soft-deprecated equivalents to the arguments above.
 #'
 #' @return A data.frame containing one field per aspect of data in \code{x} - see Details.
 #' Each row corresponds to a feature (i.e., row) of \code{x}.
@@ -17,21 +21,23 @@
 #' This function enables us to conveniently create a per-feature data.frame from a \linkS4class{SingleCellExperiment}.
 #' Each row of the returned data.frame corresponds to a row in \code{x},
 #' while each column of the data.frame corresponds to one aspect of the (meta)data in \code{x}.
+#'
 #' Columns are provided in the following order:
 #' \enumerate{
-#' \item Columns named according to values in \code{cells} represent the expression values across features for the specified cell in the \code{exprs_values} assay.
+#' \item Columns named according to the entries of \code{cells} represent the expression values across features for the specified cell in the \code{assay.type} assay.
 #' \item Columns named according to the columns of \code{rowData(x)} represent the row metadata variables.
+#' This consists of all variables if \code{use.rowdata=TRUE}, no variables if \code{use.rowdata=FALSE},
+#' and only the specified variables if \code{use.rowdata} is set to an integer or character vector.
 #' }
 #'
-#' By default, nothing is done to resolve syntactically invalid or duplicated column names;
-#' this will often lead (correctly) to an error in downstream functions like \code{\link{ggplot}}.
-#' If \code{check_names=TRUE}, this is resolved by passing the column names through \code{\link{make.names}}.
+#' By default, nothing is done to resolve syntactically invalid or duplicated column names.
+#' \code{check_names=TRUE}, this is resolved by passing the column names through \code{\link{make.names}}.
 #' Of course, as a result, some columns may not have the same names as the original fields in \code{x}.
 #'
 #' @author Aaron Lun
 #'
 #' @seealso
-#' \code{\link{ggfeatures}}, which uses this function under the hood.
+#' \code{\link{makePerCellDF}}, for the cell-level equivalent.
 #'
 #' @examples
 #' example_sce <- mockSCE()
@@ -39,27 +45,35 @@
 #' rowData(example_sce)$Length <- runif(nrow(example_sce))
 #'
 #' df <- makePerFeatureDF(example_sce, cells="Cell_001")
-#' head(colnames(df))
-#' tail(colnames(df))
-#'
-#' head(df$Cell_001)
-#' head(df$Length)
+#' head(df)
 #' 
 #' @export
 #' @importFrom SummarizedExperiment assay rowData
 #' @importFrom Matrix t
-makePerFeatureDF <- function(x, cells=NULL, exprs_values="logcounts", check_names=FALSE) {
+makePerFeatureDF <- function(x, cells=NULL, assay.type="logcounts", use.rowdata=TRUE, check.names=FALSE,
+    exprs_values=NULL, check_names=NULL)
+{
+    assay.type <- .replace(assay.type, exprs_values)
+
     # Collecting the assay values.
     keep <- colnames(x) %in% cells
-    curmat <- assay(x, exprs_values, withDimnames=FALSE)[,keep,drop=FALSE]
+    curmat <- assay(x, assay.type, withDimnames=FALSE)[,keep,drop=FALSE]
     curmat <- as.matrix(curmat)
     curmat <- data.frame(curmat, row.names=rownames(x))
     colnames(curmat) <- colnames(x)[keep]
 
-    # Adding row metadata.
-    output <- cbind(curmat, as.data.frame(rowData(x)))
+    output <- list(curmat)
 
-    if (check_names) {
+    # Collecting the row metadata.
+    use.rowdata <- .use_names_to_integer_indices(use.rowdata, x=x, nameFUN=function(x) colnames(rowData(x)), msg="use.rowdata")
+    if (length(use.rowdata)) {
+        rd <- rowData(x)[,use.rowdata,drop=FALSE]
+        output <- c(output, list(as.data.frame(rd)))
+    }
+
+    # Checking the names.
+    output <- do.call(cbind, output)
+    if (check.names) {
         colnames(output) <- make.names(colnames(output), unique=TRUE)
     }
     output
