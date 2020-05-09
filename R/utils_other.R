884df9c2d3ae24894c18b4657f69c455dc19c6b8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-05-08 22:48:27 -0700

    Ensure aggregatedAcrossCells respects average='median'. Cut down on the number of explicit arguments in the SCE method. Shuffled generally used internal into a separate file. Cleaned up the documentation.

--- R/utils_other.R
+++ R/utils_other.R
@@ -1,3 +1,21 @@
 .replace <- function(new, old) {
     if (!is.null(old)) old else new
 }
+
+.use_names_to_integer_indices <- function(use, x, nameFUN, msg) {
+    if (isTRUE(use)) {
+        use <- seq_along(nameFUN(x))
+    } else if (isFALSE(use)) {
+        use <- integer(0)
+    } else if (is.character(use)) {
+        use <- match(use, nameFUN(x))
+        if (any(is.na(use))) {
+            stop(sprintf("'%s' contains invalid values", msg))
+        }
+    } else {
+        if (any(use < 1L) || any(use > length(nameFUN(x)))) {
+            stop(sprintf("'%s' contains out-of-bounds indices", msg))
+        }
+    }
+    use
+}
