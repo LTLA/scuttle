1f2af5018ed11b6a22c4ffbd81e14b2e2c8bd389
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-08-15 17:13:49 -0700

    Generalized quickPerCellQC to work directly on SEs. Added sub.fields=TRUE to auto-determine relevant fields. Added tests for quickPerCellQC.

--- R/quickPerCellQC.R
+++ R/quickPerCellQC.R
@@ -2,11 +2,20 @@
 #'
 #' A convenient utility that identifies low-quality cells based on frequently used QC metrics.
 #'
-#' @param df A \linkS4class{DataFrame} containing per-cell QC statistics, as computed by \code{\link{perCellQCMetrics}}.
-#' @param sum.field String specifying the column of \code{df} containing the library size for each cell.
-#' @param detected.field String specifying the column of \code{df} containing the number of detected features per cell.
-#' @param sub.fields Character vector specifying the column(s) of \code{df} containing the percentage of counts in subsets of \dQuote{control features}, usually mitochondrial genes or spike-in transcripts.
-#' @param ... Further arguments to pass to \code{\link{isOutlier}}.
+#' @param x A \linkS4class{DataFrame} containing per-cell QC statistics, as computed by \code{\link{perCellQCMetrics}}.
+#' Alternatively, a \linkS4class{SummarizedExperiment} object that can be used to create such a DataFrame via \code{\link{perCellQCMetrics}}.
+#' @param sum.field String specifying the column of \code{x} containing the library size for each cell.
+#' @param detected.field String specifying the column of \code{x} containing the number of detected features per cell.
+#' @param sub.fields Character vector specifying the column(s) of \code{x} containing the percentage of counts in subsets of \dQuote{control features}, usually mitochondrial genes or spike-in transcripts.
+#'
+#' If set to \code{TRUE}, this will default to all columns in \code{x} with names following the patterns \code{"subsets_.*_percent"} and \code{"altexps_.*_percent"}. 
+#' @param ... For the generic, further arguments to pass to specific methods.
+#' 
+#' For the ANY method, further arguments to pass to \code{\link{isOutlier}}.
+#'
+#' For the SummarizedExperiment method, further arguments to pass to the ANY method.
+#' @param subsets,assay.type Arguments to pass to the \code{\link{perCellQCMetrics}} function, exposed here for convenience.
+#' @param other.args A named list containing other arguments to pass to the \code{\link{perCellQCMetrics}} function.
 #' @param lib_size,n_features,percent_subsets Soft-deprecated equivalents of the arguments above. 
 #'
 #' @return
@@ -15,7 +24,7 @@
 #' with the final \code{discard} column indicating whether the cell should be discarded.
 #'
 #' @details
-#' This function simply calls \code{\link{isOutlier}} on the various QC metrics in \code{df}.
+#' The ANY method simply calls \code{\link{isOutlier}} on the various QC metrics in \code{x}.
 #' \itemize{
 #' \item For \code{sum.field}, small outliers are detected. 
 #' These are considered to represent low-quality cells that have not been insufficiently sequenced.
@@ -33,13 +42,16 @@
 #' Users can control the outlier detection (e.g., change the number of MADs, specify batches)
 #' by passing appropriate arguments to \code{...}.
 #'
+#' The SummarizedExperiment method calls \code{\link{perCellQCMetrics}} before running outlier detection.
+#' This is simply a convenient wrapper that avoids the need for two separate function calls in routine applications.
+#'
 #' @author Aaron Lun
 #'
 #' @examples
 #' example_sce <- mockSCE()
-#' df <- perCellQCMetrics(example_sce, subsets=list(Mito=1:100))
+#' x <- perCellQCMetrics(example_sce, subsets=list(Mito=1:100))
 #'
-#' discarded <- quickPerCellQC(df, percent_subsets=c(
+#' discarded <- quickPerCellQC(x, percent_subsets=c(
 #'     "subsets_Mito_percent", "altexps_Spikes_percent"))
 #' colSums(as.data.frame(discarded))
 #'
@@ -48,8 +60,11 @@
 #'
 #' \code{\link{isOutlier}}, to identify outliers with a MAD-based approach.
 #' @export
+#' @name quickPerCellQC
+NULL
+
 #' @importFrom S4Vectors DataFrame
-quickPerCellQC <- function(df, sum.field="sum", detected.field="detected", sub.fields=NULL, 
+.quickPerCellQC <- function(x, sum.field="sum", detected.field="detected", sub.fields=NULL, 
     ..., lib_size=NULL, n_features=NULL, percent_subsets=NULL)
 {
     sum.field <- .replace(sum.field, lib_size)
@@ -57,15 +72,37 @@ quickPerCellQC <- function(df, sum.field="sum", detected.field="detected", sub.f
     sub.fields <- .replace(sub.fields, percent_subsets)
 
     output <- DataFrame(
-        low_lib_size=isOutlier(df[[sum.field]], log=TRUE, type="lower", ...),
-        low_n_features=isOutlier(df[[detected.field]], log=TRUE, type="lower", ...)
+        low_lib_size=isOutlier(x[[sum.field]], log=TRUE, type="lower", ...),
+        low_n_features=isOutlier(x[[detected.field]], log=TRUE, type="lower", ...)
     )
 
+    if (isTRUE(sub.fields)) {
+        sub.fields <- colnames(x)[
+            grepl("^subsets_.*_percent", colnames(x)) |
+            grepl("^altexps_.*_percent", colnames(x))
+        ]
+    }
+
     for (i in sub.fields) {
-        output[[paste0("high_", i)]] <- isOutlier(df[[i]], type="higher", ...)
+        output[[paste0("high_", i)]] <- isOutlier(x[[i]], type="higher", ...)
     }
 
     discard <- Reduce("|", output)
     output$discard <- discard
     output
 }
+
+#' @export
+#' @rdname quickPerCellQC
+setGeneric("quickPerCellQC", function(x, ...) standardGeneric("quickPerCellQC"))
+
+#' @export
+#' @rdname quickPerCellQC
+setMethod("quickPerCellQC", "ANY", .quickPerCellQC)
+
+#' @export
+#' @rdname quickPerCellQC
+setMethod("quickPerCellQC", "SummarizedExperiment", function(x, ..., subsets=NULL, assay.type="counts", other.args=list()) {
+    x <- do.call(perCellQCMetrics, c(list(x, subsets=subsets, assay.type=assay.type), other.args))
+    .quickPerCellQC(x, ...)
+})
