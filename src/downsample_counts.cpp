193fd163e4f6a2e71eaef4bf6577d02c30229dd5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-09-27 22:40:02 -0700

    More efficient creation of downsampled matrix.

--- src/downsample_counts.cpp
+++ src/downsample_counts.cpp
@@ -1,6 +1,7 @@
 #include "Rcpp.h"
 #include "beachmat3/beachmat.h"
 #include "scuttle/downsample_vector.h"
+#include <deque>
 
 // [[Rcpp::export]]
 Rcpp::RObject downsample_column(Rcpp::RObject input, Rcpp::NumericVector prop) {
@@ -13,7 +14,7 @@ Rcpp::RObject downsample_column(Rcpp::RObject input, Rcpp::NumericVector prop) {
         auto smat = beachmat::promote_to_sparse(mat);
         std::vector<int> work_i(ngenes);
         std::vector<double> work_x(ngenes);
-        std::map<std::pair<int, int>, double> store;
+        std::deque<std::pair<std::pair<int, int>, double> > store;
 
         auto pIt=prop.begin();
         for (size_t i=0; i<ncells; ++i, ++pIt) {
@@ -21,11 +22,12 @@ Rcpp::RObject downsample_column(Rcpp::RObject input, Rcpp::NumericVector prop) {
             scuttle::downsample_vector(indices.x, indices.x + indices.n, work_x.begin(), *pIt);
             for (size_t j = 0; j < indices.n; ++j) {
                 if (work_x[j]) {
-                    store[std::make_pair(i, indices.i[j])] = work_x[j];
+                    store.push_back(std::make_pair(std::make_pair(i, indices.i[j]), work_x[j]));
                 }
             }
         }
 
+        // 'store' is already sorted, from the sparse_index contract.
         return beachmat::as_gCMatrix<Rcpp::NumericVector>(ngenes, ncells, store);
 
     } else {
@@ -57,7 +59,7 @@ Rcpp::RObject downsample_matrix(Rcpp::RObject rmat, double total, double require
         auto smat = beachmat::promote_to_sparse(mat);
         std::vector<int> work_i(ngenes);
         std::vector<double> work_x(ngenes);
-        std::map<std::pair<int, int>, double> store;
+        std::deque<std::pair<std::pair<int, int>, double> > store;
 
         for (size_t i=0; i<ncells; ++i) {
             auto indices = smat->get_col(i, work_x.data(), work_i.data());
@@ -68,11 +70,12 @@ Rcpp::RObject downsample_matrix(Rcpp::RObject rmat, double total, double require
             downsampler(indices.x, indices.x + indices.n, work_x.begin());
             for (size_t j = 0; j < indices.n; ++j) {
                 if (work_x[j]) {
-                    store[std::make_pair(i, indices.i[j])] = work_x[j];
+                    store.push_back(std::make_pair(std::make_pair(i, indices.i[j]), work_x[j]));
                 }
             }
         }
 
+        // 'store' is already sorted, from the sparse_index contract.
         output = beachmat::as_gCMatrix<Rcpp::NumericVector>(ngenes, ncells, store);
 
     } else {
