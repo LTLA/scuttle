fdf2c31b8b2c7585df69033f6d2577f17022a21c
Lluís Revilla <lrevilla@irsicaixa.es>
2024-07-02 16:20:05 +0200

    Add test for all types of subsets

--- tests/testthat/test-qc-calc.R
+++ tests/testthat/test-qc-calc.R
@@ -201,3 +201,28 @@ test_that("we can compute standard QC metrics across multiple cores", {
     expect_equal(perFeatureQCMetrics(original, subset=list(set=1:10), flatten=FALSE), 
         perFeatureQCMetrics(original, subset=list(set=1:10), BPPARAM=safeBPParam(3), flatten=FALSE))
 })
+
+#######################################################################
+# addPerCellQCMetrics works: 
+test_that("addPerCellQCMetrics adds rowData columns", {
+    # Three types of using subsets
+    genes_numeric <- 1:5
+    genes_names <- c("Gene_0001", "Gene_2000")
+    genes_logical <- vector("logical", nrow(original))
+    genes_logical[sample(nrow(original), 5)] <- TRUE
+    
+    subsets <- list(numeric = genes_numeric,
+                    logical = genes_logical,
+                    names = genes_names)
+    out_sce <- addPerCellQCMetrics(original, subsets =  subsets)
+    rd <- rowData(out_sce)
+    expect_equal(ncol(rd), length(subsets))
+    expect_equal(colnames(rd), names(subsets))
+    expect_equal(colSums(as.matrix(rd[, names(subsets)])),
+                 c("numeric" = length(genes_numeric), 
+                   "logical" = sum(genes_logical), 
+                   names = length(genes_names)))
+    
+    # colData is modified
+    expect_gt(ncol(colData(out_sce)), 3)
+})
